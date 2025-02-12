1c0f45c5229f91881778833a078dd14cab62fdcf
Lucas <lucas.ramos46@fatec.sp.gov.br>
2025-02-12 17:26:55 -0300

    learn(ap01-js): encerra a secao 1.1
    -vimos var, let e const

---
+++ intro_js/script.js
@@ -0,0 +1,33 @@
+// pq nao usar o var
+var idade = 18
+console.log('Oi,' + nome)
+//iça,emtp (hoist)
+if(idade >= 18){
+    var nome = 'João'
+    console.log('Parabéns,' + nome + ', Você pode dirigir')
+}
+console.log ("Até mais," + nome)
+
+//declarando constantes
+
+// var a = 2
+// console.log(a)
+// a = 3
+// console.log(a)
+
+// let b 
+// console.log(b)
+
+
+// let a = 2
+// console.log(a)
+// a = 3
+// console.log(a)
+
+
+
+// const idade = 27 
+
+//java: String nome = "José";
+// const nome = 'José'
+// console.log(nome)
