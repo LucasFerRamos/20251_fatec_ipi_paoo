7dc64c2c365f5855960a95d0a7d6304497654ca3
Lucas <lucas.ramos46@fatec.sp.gov.br>
2025-04-02 18:05:35 -0300

    learn(ap01-mss):4.3.1

---
+++ microsservicos/lembretes/index.js
@@ -0,0 +1,13 @@
+const express= require('express')
+const app = express()
+//localhost:4000/lembretes
+
+//GET /lembretes () => {} (endpoint)
+//rec request ---- res resposta
+app.get('/lembretes', (req,res) => {
+
+
+})
+
+//POST /lembretes () => {} (endpoint)
+app.post('/lembretes', (req,res) => {})
